003244e3ce1ebf4f6631d97ece672fb013e965e0
Frilo8619 <133773533+Frilo8619@users.noreply.github.com>
2025-03-23 20:56:34 +0100

    Update article.js

--- article.js
+++ article.js
@@ -1,38 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    data.forEach((element)=> {
-        console.log(element);
-        const messageTemplateElement = document.querySelector('#template');
-        let newElement = messageTemplateElement.cloneNode(true).content;
-        newElement.querySelector('#image').src = "medias/images/" + element.picture;
-        newElement.querySelector('#name').textContent = element.name;
-        newElement.querySelector('#description').textContent = element.description;
-        newElement.querySelector('#price').textContent = element.price+"€";
-        newElement.querySelector('#bouton').textContent = "Ajouter Produit";
-        corps3.appendChild(newElement);
+    const boutonNav = document.querySelector(".butonav");
+
+    data.forEach((element) => {
+        const newElement = document.querySelector("#template").content.cloneNode(true);
+        newElement.querySelector("#image").src = "medias/images/" + element.picture;
+        newElement.querySelector("#name").textContent = element.name;
+        newElement.querySelector("#description").textContent = element.description;
+        newElement.querySelector("#price").textContent = element.price + "€";
+        newElement.querySelector("#bouton").textContent = "Ajouter Produit";
+        boutonNav.before(newElement);
     });
-    
-})
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch("https://iut.nathanael-spriet.fr/data.json")
-    .then((reponse) => reponse.json())
-    .then((json) => {json.forEach((ele)=> {
-    
-    const messageTemplateElement = document.querySelector('#template');
-    let newElement = messageTemplateElement.cloneNode(true).content;
-    newElement.querySelector('#image').src = "medias/images/" + ele.picture;
-    newElement.querySelector('#name').textContent = ele.name;
-    newElement.querySelector('#description').textContent = ele.description;
-    newElement.querySelector('#price').textContent = ele.price + "€";
-    newElement.querySelector('#bouton').textContent = "Ajouter Produit";
-    corps3.appendChild(newElement);
-
-
-
-})});
-})
-
-
-
-
+        .then(response => response.json())
+        .then(json => {
+            const boutonNav = document.querySelector(".butonav");
+            
+            json.forEach(ele => {
+                const newElement = document.querySelector("#template").content.cloneNode(true);
+                newElement.querySelector("#image").src = "medias/images/" + ele.picture;
+                newElement.querySelector("#name").textContent = ele.name;
+                newElement.querySelector("#description").textContent = ele.description;
+                newElement.querySelector("#price").textContent = ele.price + "€";
+                newElement.querySelector("#bouton").textContent = "Ajouter Produit";
+                boutonNav.before(newElement); 
+            });
+        });
+});
