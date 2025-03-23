5a7d18768a09d9886967d00219b858a2cb418891
doisyn <noa.doisy@etu.univ-grenoble-alpes.fr>
2025-03-23 22:21:53 +0100

    Update article.js
    Ajout du premier Bonus ( chargement )

--- article.js
+++ article.js
@@ -1,32 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const boutonNav = document.querySelector(".butonav");
+    setTimeout(() => {
+        const boutonNav = document.querySelector(".butonav");
 
-    data.forEach((element) => {
-        const newElement = document.querySelector("#template").content.cloneNode(true);
-        newElement.querySelector("#image").src = "medias/images/" + element.picture;
-        newElement.querySelector("#name").textContent = element.name;
-        newElement.querySelector("#description").textContent = element.description;
-        newElement.querySelector("#price").textContent = element.price + "€";
-        newElement.querySelector("#bouton").textContent = "Ajouter Produit";
-        boutonNav.before(newElement);
-    });
+        data.forEach((element) => {
+            const newElement = document.querySelector("#template").content.cloneNode(true);
+            newElement.querySelector("#image").src = "medias/images/" + element.picture;
+            newElement.querySelector("#name").textContent = element.name;
+            newElement.querySelector("#description").textContent = element.description;
+            newElement.querySelector("#price").textContent = element.price + "€";
+            newElement.querySelector("#bouton").textContent = "Ajouter Produit";
+            boutonNav.before(newElement);
+        });
+    }, 2000); 
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://iut.nathanael-spriet.fr/data.json")
-        .then(response => response.json())
-        .then(json => {
-            const boutonNav = document.querySelector(".butonav");
-            
-            json.forEach(ele => {
-                const newElement = document.querySelector("#template").content.cloneNode(true);
-                newElement.querySelector("#image").src = "medias/images/" + ele.picture;
-                newElement.querySelector("#name").textContent = ele.name;
-                newElement.querySelector("#description").textContent = ele.description;
-                newElement.querySelector("#price").textContent = ele.price + "€";
-                newElement.querySelector("#bouton").textContent = "Ajouter Produit";
-                boutonNav.before(newElement); 
+    setTimeout(() => {
+        fetch("https://iut.nathanael-spriet.fr/data.json")
+            .then(response => response.json())
+            .then(json => {
+                const boutonNav = document.querySelector(".butonav");
+                
+                json.forEach(ele => {
+                    const newElement = document.querySelector("#template").content.cloneNode(true);
+                    newElement.querySelector("#image").src = "medias/images/" + ele.picture;
+                    newElement.querySelector("#name").textContent = ele.name;
+                    newElement.querySelector("#description").textContent = ele.description;
+                    newElement.querySelector("#price").textContent = ele.price + "€";
+                    newElement.querySelector("#bouton").textContent = "Ajouter Produit";
+                    boutonNav.before(newElement); 
+                });
             });
-        });
+    }, 2000);
 });
